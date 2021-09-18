504a7062bb6a6c4123fa4e19453a25951a740541
penguin027 <1820032283@qq.com>
2021-09-18 14:52:03 +0800

    upgrade9
    使用正则表达式去除注释部分以及引号内的内容

--- countKey.py
+++ countKey.py
@@ -1,4 +1,4 @@
-import os
+﻿import os
 import re
 import time
 
@@ -7,26 +7,11 @@ keyWords=["auto","break","case","char","const","continue","default","do",
          "int","long","register","return","short","signed","sizeof","static",
          "struct","switch","typedef","union","unsigned","void","volatile","while"]
 
-#把代码文件内的注释部分以及print(）内的内容删除
+#把代码文件内的注释部分以及引号内的内容删除
 def remove(str):
-    index=0     #记录注释开始的位置
-    if(len(str)>=2):
-        for i in range(1,len(str)):     #去除注释部分
-            if str[i]=='/' and str[i-1]=='/':
-                index=i
-                break
-        str=str[:index-1]
-
-    index=0     #记录print开始的位置
-    if(len(str)>=5):
-        for i in range(0,len(str)-4):     #去掉print内的内容
-            if (str[i]=='p' and str[i+1]=='r' and str[i+2]=='i'
-                and str[i+3]=='n' and str[i+4]=='t'):
-                index=i
-                break
-            else:
-                index=-1
-        str=str[:index]
+    str = re.sub('(?<!:)\\/\\/.*|\\/\\*(\\s|.)*?\\*\\/',' ',str)    # 删除注释部分
+    str = re.sub('/(?<=").*?(?=")/',' ',str)        # 删除带双引号的内容
+    str = re.sub("/(?<=').*?(?=')/", ' ', str)  # 删除带单引号的内容
     return str
 
 #用正则表达式将非英文字符替换为空
@@ -36,16 +21,17 @@ def removeNonLetter(str):
 #把文件内的单词分割为列表
 def splitWord(dir):
     lis = []  # 存放所有的单词
-    f = open(dir, 'r', encoding='utf-8')
+    f = open(dir, 'r', encoding='gbk')
     lines = f.readlines()
+    new_line=""
     for line in lines:
         if line != '' and line != '\n':  # 过滤掉空行
-            new_line = remove(line)       # 去除注释部分以及print()的内容
-            new_line = removeNonLetter(new_line)    # 去除非字母部分
-            new_line = new_line.strip()  # 去除前后空格
-            li = new_line.split(" ")    #每一行分割成列表
-        for i in li:
-            lis.append(i)       #汇总成一个列表
+            new_line = new_line + line      # 把文件内容变成一个字符串
+
+    new_line = remove(new_line)  # 去除注释部分以及print()的内容
+    new_line = removeNonLetter(new_line)  # 去除非字母部分
+    new_line = new_line.strip()  # 去除前后空格
+    lis = new_line.split(" ")  # 分割成列表
     return lis
 
 # 删除列表中的空值
@@ -123,7 +109,14 @@ def CountKey(dir,rank):
                     if (lis[j] == 'case'):
                         case += 1
                 case_num.append(case)
-        print("case num: {0}".format(case_num))
+
+        if(len(case_num)==0):
+            print("case num: 0")
+        else:
+            print("case num: ",end='')
+            for num in case_num:
+                print(str(num)+" ",end='')
+            print()
 
     #rank3:终极要求：输出有几组if-else结构
     def rank_3():
@@ -136,26 +129,28 @@ def CountKey(dir,rank):
         if_else = merge_if_else(ifelse)       # 逐步进行合并 先配对if-else
         if_elseif_else = merge_if_elseif_else(if_else)    #再配对if-elseif-else
 
-        while("if-elseif-else" in if_elseif_else):  # 统计if-elseif-else
-            for i in range(len(if_elseif_else)):
-                if if_elseif_else[i]=="if-elseif-else":
-                    if_elseif_else[i]=""
+        if(len(if_elseif_else)!=0):
+            while("if-elseif-else" in if_elseif_else):  # 统计if-elseif-else
+                for i in range(len(if_elseif_else)):
+                    if if_elseif_else[i]=="if-elseif-else":
+                        if_elseif_else[i]=""
 
-            if_elseif_else=delEmpty(if_elseif_else)     # 删除列表中的空值
-            if(len(if_elseif_else)!=0):
-                if_else=merge_if_else(if_elseif_else)   #再次进行合并
-                if_elseif_else=merge_if_elseif_else(if_else)
+                if_elseif_else=delEmpty(if_elseif_else)     # 删除列表中的空值
+                if(len(if_elseif_else)!=0):
+                    if_else=merge_if_else(if_elseif_else)   #再次进行合并
+                    if_elseif_else=merge_if_elseif_else(if_else)
 
-        if_else = merge_if_else(if_elseif_else)     #对统计完if-elseif-else的列表进行合并
-        while ("if-else" in if_else):  # 统计if-else的数目
-            for i in range(len(if_else)):
-                if if_else[i] == "if-else":
-                    if_else[i] = ""
-                    if_else_num += 1
+        if(len(if_elseif_else)!=0):
+            if_else = merge_if_else(if_elseif_else)     #对统计完if-elseif-else的列表进行合并
+            while ("if-else" in if_else):  # 统计if-else的数目
+                for i in range(len(if_else)):
+                    if if_else[i] == "if-else":
+                        if_else[i] = ""
+                        if_else_num += 1
 
-            if_else = delEmpty(if_else)  # 删除列表中的空值
-            if(len(if_else)!=0):
-                if_else = merge_if_else(if_else)  # 再次进行合并
+                if_else = delEmpty(if_else)  # 删除列表中的空值
+                if(len(if_else)!=0):
+                    if_else = merge_if_else(if_else)  # 再次进行合并
         print("if-else num: {0}".format(if_else_num))  # if-else的数目
 
     #rank4:拔高要求：输出有几组if-elseif-else结构
@@ -169,16 +164,18 @@ def CountKey(dir,rank):
         if_else = merge_if_else(ifelse)  # 逐步进行合并 先配对if-else
         if_elseif_else = merge_if_elseif_else(if_else)  # 再配对if-elseif-else
 
-        while ("if-elseif-else" in if_elseif_else):  # 统计if-elseif-else的数目
-            for i in range(len(if_elseif_else)):
-                if if_elseif_else[i] == "if-elseif-else":
-                    if_elseif_else[i] = ""
-                    if_elseif_else_num += 1
+        if(len(if_elseif_else)!=0):
+            while ("if-elseif-else" in if_elseif_else):  # 统计if-elseif-else的数目
+                for i in range(len(if_elseif_else)):
+                    if if_elseif_else[i] == "if-elseif-else":
+                        if_elseif_else[i] = ""
+                        if_elseif_else_num += 1
 
-            if_elseif_else = delEmpty(if_elseif_else)  # 删除列表中的空值
-            if (len(if_elseif_else) != 0):
-                if_else = merge_if_else(if_elseif_else)  # 再次进行合并
-                if_elseif_else = merge_if_elseif_else(if_else)
+                if_elseif_else = delEmpty(if_elseif_else)  # 删除列表中的空值
+                if (len(if_elseif_else) != 0):
+                    if_else = merge_if_else(if_elseif_else)  # 再次进行合并
+                    if_elseif_else = merge_if_elseif_else(if_else)
+
         print("if-elseif-else num: {0}".format(if_elseif_else_num))  # if-elseif-else的数目
 
     if rank==1:
@@ -205,5 +202,5 @@ if __name__ == '__main__':
     #basedir=r"C:\Users\MAC\Desktop\test.cpp"
     start_time = time.time()
     CountKey(basedir,int(rank))
-    #print(time.time()-start_time)
+    print("\nRun Time: {0} seconds".format(time.time()-start_time))
 
