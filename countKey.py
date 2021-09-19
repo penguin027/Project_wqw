4a6dd97f2a8d9c3c5703586ab0119f3078b1effe
penguin027 <1820032283@qq.com>
2021-09-19 11:26:47 +0800

    upgrade10
    修改了正则表达式匹配引号内容的错误

--- countKey.py
+++ countKey.py
@@ -1,5 +1,6 @@
 ﻿import os
 import re
+from cProfile import Profile
 import time
 
 keyWords=["auto","break","case","char","const","continue","default","do",
@@ -10,35 +11,35 @@ keyWords=["auto","break","case","char","const","continue","default","do",
 #把代码文件内的注释部分以及引号内的内容删除
 def remove(str):
     str = re.sub('(?<!:)\\/\\/.*|\\/\\*(\\s|.)*?\\*\\/',' ',str)    # 删除注释部分
-    str = re.sub('/(?<=").*?(?=")/',' ',str)        # 删除带双引号的内容
-    str = re.sub("/(?<=').*?(?=')/", ' ', str)  # 删除带单引号的内容
+    str = re.sub('\"([^\"]*)\"',' ',str)        # 删除带双引号的内容
+    str = re.sub("\'([^\']*)\'", ' ', str)  # 删除带单引号的内容
     return str
 
 #用正则表达式将非英文字符替换为空
-def removeNonLetter(str):
+def remove_non_letter(str):
     return re.sub('[^a-zA-Z]',' ',str)
 
+# 删除列表中的空值
+def del_empty(list):
+    while '' in list:
+        list.remove('')
+    return list
+
 #把文件内的单词分割为列表
-def splitWord(dir):
-    lis = []  # 存放所有的单词
+def split_word(dir):
     f = open(dir, 'r', encoding='gbk')
     lines = f.readlines()
-    new_line=""
+    new_line=""     # 用于记录文件内容的字符串
     for line in lines:
         if line != '' and line != '\n':  # 过滤掉空行
             new_line = new_line + line      # 把文件内容变成一个字符串
 
-    new_line = remove(new_line)  # 去除注释部分以及print()的内容
-    new_line = removeNonLetter(new_line)  # 去除非字母部分
+    new_line = remove(new_line)  # 去除注释部分以及引号的内容
+    new_line = remove_non_letter(new_line)  # 去除非字母部分
     new_line = new_line.strip()  # 去除前后空格
     lis = new_line.split(" ")  # 分割成列表
+    lis = del_empty(lis) # 删除列表中空值
     return lis
-
-# 删除列表中的空值
-def delEmpty(list):
-    while '' in list:
-        list.remove('')
-    return list
 
 # 把if和对应else进行配对
 def merge_if_else(list):
@@ -72,61 +73,54 @@ def merge_if_elseif_else(list):
     return if_elseif_else
 
 #按等级计算关键字字数
-def CountKey(dir,rank):
-    lis = splitWord(dir)  # 获取单词列表
-    word_list = []  # 存放关键字
-
+def count_key(dir,rank):
+    word_list = split_word(dir)  # 获取单词列表
+    keyword_list = []
     #rank1基础要求：输出关键字统计信息
     def rank_1():
-        for word in lis:
-            if word in keyWords:        #提取出关键字并存放到word_list中
-                word_list.append(word)
-                #continue
-        print("total num: {0}".format(len(word_list)))
+        for word in word_list:
+            if word in keyWords:  # 提取出关键字并存放到word_list中
+                keyword_list.append(word)
+                # continue
+        print("total num: {0}".format(len(keyword_list)))
 
     # rank2进阶要求：输出有几组switch case结构，同时输出每组对应的case个数
     def rank_2():
-        i = 0
-        switch_index = []  # 记录switch所在的下标
-        case_num = []  # 记录case的数量
-        switch_num=0    # 记录switch的数量
-        for word in lis:
-            if word == "switch":
+        switch_case=[]
+        for word in word_list:
+            if word in ['switch','case']:        #提取出switch,case并存放到switch_case[ ]中
+                switch_case.append(word)
+        switch_num = 0  # 记录switch的数目
+        case_num = []   # 记录case的数目
+        for i in range(len(switch_case)):
+            if switch_case[i] == "switch":
                 switch_num += 1
-                switch_index.append(i)  # 记录switch的下标值
-            i += 1
+                temp_case_num = 0
+                for j in range(i+1,len(switch_case)):
+                    if switch_case[j] == "case":
+                        temp_case_num += 1
+                    else:
+                        break
+                case_num.append(temp_case_num)
+
         print("switch num: {0}".format(switch_num))
-
-        for i in range(len(switch_index)):
-            case = 0
-            if i != len(switch_index) - 1:
-                for j in range(switch_index[i], switch_index[i + 1]):
-                    if (lis[j] == 'case'):
-                        case += 1
-                case_num.append(case)
-            if i == len(switch_index) - 1:
-                for j in range(switch_index[len(switch_index) - 1], len(lis)):
-                    if (lis[j] == 'case'):
-                        case += 1
-                case_num.append(case)
-
-        if(len(case_num)==0):
+        if (len(case_num) == 0):
             print("case num: 0")
         else:
-            print("case num: ",end='')
+            print("case num: ", end='')
             for num in case_num:
-                print(str(num)+" ",end='')
+                print(str(num) + " ", end='')
             print()
 
-    #rank3:终极要求：输出有几组if-else结构
+    #rank3:拔高要求：输出有几组if-else结构
     def rank_3():
-        ifelse = []  # 记录所有单个if，else的列表
+        ifOrElse = []  # 记录所有单个if，else的列表
         if_else_num=0   #记录if-else的数量
-        for word in lis:
+        for word in word_list:
             if word=="if" or word=="else":
-                ifelse.append(word)     # 把所有的if else单独提取出来
+                ifOrElse.append(word)     # 把所有的if else单独提取出来
 
-        if_else = merge_if_else(ifelse)       # 逐步进行合并 先配对if-else
+        if_else = merge_if_else(ifOrElse)       # 逐步进行合并 先配对if-else
         if_elseif_else = merge_if_elseif_else(if_else)    #再配对if-elseif-else
 
         if(len(if_elseif_else)!=0):
@@ -135,7 +129,7 @@ def CountKey(dir,rank):
                     if if_elseif_else[i]=="if-elseif-else":
                         if_elseif_else[i]=""
 
-                if_elseif_else=delEmpty(if_elseif_else)     # 删除列表中的空值
+                if_elseif_else=del_empty(if_elseif_else)     # 删除列表中的空值
                 if(len(if_elseif_else)!=0):
                     if_else=merge_if_else(if_elseif_else)   #再次进行合并
                     if_elseif_else=merge_if_elseif_else(if_else)
@@ -148,20 +142,20 @@ def CountKey(dir,rank):
                         if_else[i] = ""
                         if_else_num += 1
 
-                if_else = delEmpty(if_else)  # 删除列表中的空值
+                if_else = del_empty(if_else)  # 删除列表中的空值
                 if(len(if_else)!=0):
                     if_else = merge_if_else(if_else)  # 再次进行合并
         print("if-else num: {0}".format(if_else_num))  # if-else的数目
 
-    #rank4:拔高要求：输出有几组if-elseif-else结构
+    #rank4:终极要求：输出有几组if-elseif-else结构
     def rank_4():
-        ifelse = []  # 记录所有单个if，else的列表
+        ifOrElse = []  # 记录所有单个if，else的列表
         if_elseif_else_num = 0  # 记录if-elseif-else的数量
-        for word in lis:
+        for word in word_list:
             if word == "if" or word == "else":
-                ifelse.append(word)   # 把所有的if else单独提取出来
+                ifOrElse.append(word)   # 把所有的if else单独提取出来
 
-        if_else = merge_if_else(ifelse)  # 逐步进行合并 先配对if-else
+        if_else = merge_if_else(ifOrElse)  # 逐步进行合并 先配对if-else
         if_elseif_else = merge_if_elseif_else(if_else)  # 再配对if-elseif-else
 
         if(len(if_elseif_else)!=0):
@@ -171,7 +165,7 @@ def CountKey(dir,rank):
                         if_elseif_else[i] = ""
                         if_elseif_else_num += 1
 
-                if_elseif_else = delEmpty(if_elseif_else)  # 删除列表中的空值
+                if_elseif_else = del_empty(if_elseif_else)  # 删除列表中的空值
                 if (len(if_elseif_else) != 0):
                     if_else = merge_if_else(if_elseif_else)  # 再次进行合并
                     if_elseif_else = merge_if_elseif_else(if_else)
@@ -196,11 +190,9 @@ def CountKey(dir,rank):
         rank_3()
         rank_4()
 
-
 if __name__ == '__main__':
-    basedir,rank=input("请输入文件路径 完成等级:\n").split(" ")
-    #basedir=r"C:\Users\MAC\Desktop\test.cpp"
+    basedir,rank=input("").split(" ")
     start_time = time.time()
-    CountKey(basedir,int(rank))
+    count_key(basedir,int(rank))
+    time.sleep(1)
     print("\nRun Time: {0} seconds".format(time.time()-start_time))
-
